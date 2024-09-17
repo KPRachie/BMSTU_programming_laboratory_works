053187ab30b93049a350c2080c78855457dc8fb1
User <kathianikina1104@gmail.com>
2024-09-17 19:18:36 +0300

    2_lab added

--- BMSTU_programming_laboratory_works/BMSTU_programming_laboratory_works.cpp
+++ BMSTU_programming_laboratory_works/BMSTU_programming_laboratory_works.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//ЛР 3 Программирование циклического процесса.Типы циклов.
+//Рассмотреть решение предложенной задачи с использованием всех трех видов
+//циклов.Отладить программу с наиболее рациональным вариантом цикла.Обосновать
+//выбор.
+//В заданном целом числе определить количество нечетных цифр.
+
+
+int main() {
+    int counter = 0;
+    std::string st;
+    std::cout << "Please, enter your number: ";
+    std::cin >> st;
+    for (char& s : st) {
+        if ((int(s) - '0') % 2 != 0) {
+            counter++;
+        }
+    }
+    //int n = st.length();
+    //int i = 0;
+    //while (i < n) {
+    //    if ((int(st[i]) - '0') % 2 != 0) {
+    //        counter++;
+    //    }
+    //    i++;
+    //}
+    //int n = st.length();
+    //int i = 0;
+    //do {
+    //    if ((int(st[i]) - '0') % 2 != 0) {
+    //        counter++;
+    //    }
+    //    i++;
+    //} while (i < n);
+    std::cout << counter;
+    return 0;
 }
+
+
