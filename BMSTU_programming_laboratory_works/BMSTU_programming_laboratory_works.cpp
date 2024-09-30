28d3cef902f446520a74db1d1772b7d098c6df9b
User <kathianikina1104@gmail.com>
2024-09-30 08:53:07 +0300

    added 6_lab.

--- BMSTU_programming_laboratory_works/BMSTU_programming_laboratory_works.cpp
+++ BMSTU_programming_laboratory_works/BMSTU_programming_laboratory_works.cpp
@@ -1,6 +1,28 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+//ЛР 8 Файловая система.
+//Создать файл F, состоящий из целых чисел. Переписать из файла F в файл G все
+//нечетные числа.
+
+
+int main() {
+    std::string line;
+    std::ofstream myfilein("G.txt"); 
+    std::ifstream myfileout("F.txt");
+    if (myfilein.is_open() and myfileout.is_open()) {
+        while (getline(myfileout, line)) {
+            for (auto& a : line) {
+                if ((((int) a) - '0') % 2 != 0) {
+                    myfilein << a - '0';
+                }
+            }
+        }
+        myfilein.close();
+        myfileout.close();
+    }
+    return 0;
+
 }
